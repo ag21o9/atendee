d1c4c890f91824b984e8359aa091c6037fda372a
Abhijeet <iamgenious21o9@gmail.com>
2024-06-02 20:54:26 +0530

    completed backend

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,43 @@
-var express = require('express');
-const usermodel = require('./users');
+var express = require("express");
+const usermodel = require("./users");
 var router = express.Router();
 
 /* GET home page. */
 
-
-let usercheck = {}
+let usercheck = {};
 const date = new Date();
 const data = date.toDateString();
 
-function check(req,res,next){
-  if(usercheck[req.headers.userID]==data){
-    res.redirect('/submitted')
-  }
-  else{
-    usercheck[req.headers.userID]=data;
-    next();
+async function check(req, res, next) {
+  const name = await usermodel.findOne({ roll: req.body.roll });
+  if (name != null) {
+    res.redirect("/")
+  } else if (req.body.name == "" || req.body.roll == "" || req.body.roll.toString().length<13) {
+    res.redirect("/");
+  } else {
+    if (usercheck[req.headers.userID] == data) {
+      res.redirect("/");
+    } else {
+      usercheck[req.headers.userID] = data;
+      next();
+    }
   }
 }
 
+router.get("/", (req, res) => {
+  res.render("index");
+});
 
-router.post('/submitted',(req,res)=>{
-  res.send("submitted already")
+router.get('/e',(req,res)=>{
+  res.render("success")
 })
 
-router.post('/create',check, async function(req, res, next) {
+router.post("/create", check, async function (req, res, next) {
   const user = await usermodel.create({
-    username : req.body.name,
-    roll : req.body.roll,
-  })
-  res.send(user);
+    username: req.body.name,
+    roll: req.body.roll,
+  });
+  res.render("Success", {users:user});
 });
 
 module.exports = router;
